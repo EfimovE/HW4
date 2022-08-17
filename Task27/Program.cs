c2273d754acfe58bd936d29aac97775747659a97
EfimovE <efimov_e@mail.ru>
2022-08-17 17:35:31 +0500

    save2

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -6,6 +6,10 @@
 
 int Sum (int number)
 {
+    if (number < 0)
+    {
+        return -1;
+    }
     int lastNumber = number % 10;
     int result = 0;
     while (number / 10 > 0)
@@ -16,8 +20,10 @@ int Sum (int number)
     return result + lastNumber; 
 }
 
+
 Console.WriteLine("Введите число и нажмите Enter.");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int res = Sum(num);
-Console.WriteLine($"Сумма цифр в числе {num} равна {res}.");
+string res1 = res == -1 ? "введите число большее, либо равное нулю" : res.ToString();
+Console.WriteLine($"Сумма цифр в числе {num} равна: {res1}.");
