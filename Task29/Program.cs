e15b46ae8a3b78f8eb1c5c54d4c1ae2c42c69172
EfimovE <efimov_e@mail.ru>
2022-08-17 17:52:45 +0500

    save1

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 
+// 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void FillArray (int[]collection)
+{
+    int lenght = collection.Length;
+    int i = 0;
+    Random rnd = new Random();
+    while (i < lenght)
+    {
+        collection[i]=rnd.Next(-45926,12345);
+        i++;
+    }
+}
+void PrintArray (int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("[ ");
+    while (position < count - 1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    Console.Write($"{col[position]} ]");
+}
+int[]array = new int [8];
+FillArray(array);
+PrintArray(array);
